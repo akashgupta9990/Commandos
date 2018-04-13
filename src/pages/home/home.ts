1c7066cf0f0438f3b7c68df2556234fc870bd8cc
akashgupta9990 <akash.gupta@tarento.com>
2018-04-13 16:46:33 +0530

    Issue #2 fix: Added home pages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { NgClass } from '@angular/common';
 import { MenuLabels } from '../../constants/menulabels';
+import { NewGameLabels } from '../../constants/menulabels';
+import { LoadGameLabels } from '../../constants/menulabels';
+import { OptionsLabels } from '../../constants/menulabels';
+import { QuitGameLabels } from '../../constants/menulabels';
 // import * as _ from "underscore";
 import { Fonts } from "../../app/fonts";
 
@@ -13,12 +15,41 @@ export class HomePage {
   mainmenu_labels = MenuLabels;
   fontsImage = Fonts;
   
-  constructor(public navCtrl: NavController) {
+  constructor() {
     this.initializeMenu();
   }
 
   initializeMenu(){
     
+  }
+
+  clickLabels(param) {
+    switch (param) {
+      case 'new game':
+        this.mainmenu_labels = NewGameLabels;
+        break;
+      case 'save game':
+        // this.mainmenu_labels = OptionsLabels;
+        break;
+      case 'load game':
+        this.mainmenu_labels = LoadGameLabels;
+        break;
+      case 'options':
+        this.mainmenu_labels = OptionsLabels;
+        break;
+      case 'credits':
+        // this.mainmenu_labels = OptionsLabels;
+        break;
+      case 'help':
+        // this.mainmenu_labels = OptionsLabels;
+        break;
+      case 'quit game':
+        this.mainmenu_labels = QuitGameLabels;
+        break;
+      default:
+        this.mainmenu_labels = MenuLabels;
+        break;
+    }
   }
 
 }
